46c64d5fa69afd45fd42238d2a705416c3e68d79
mereet <piku84400@gmail.com>
2023-07-22 17:58:42 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import  express from 'express';
+import router from './routes/routes.js';
+import cors from 'cors';
+import DBConnection from './database/db.js';
+const app=express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use('/',router);
+const PORT=5000;
+DBConnection();
+app.listen(PORT,()=>console.log(`server is running on ${PORT}`));
